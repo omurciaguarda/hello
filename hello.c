e8765fbbb9e34d6a2aa8b52b95befe2ed44aa10c
Oscar <omurciaguardado2@huskers.unl.edu>
2022-08-25 20:08:16 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,13 @@
+/**
+ * Lab Hack 01
+ * Author: Oscar Murcia Guardado and Sam Aguilar
+ * Date: 08/25/22
+ */
+
+    #include<stdlib.h>
+    #include<stdio.h>
+
+    int main(){
+    printf("Oscar Murcia Guardado\n");
+
+    }
